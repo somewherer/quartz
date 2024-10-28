e55df3aa007be6b316800cee52363212d3cd6215
i <i>
2024-10-28 17:42:34 +1100

    feature: change search keyboard shortcut

--- quartz/components/scripts/search.inline.ts
+++ quartz/components/scripts/search.inline.ts
@@ -207,7 +207,7 @@ document.addEventListener("nav", async (e: CustomEventMap["nav"]) => {
   let currentHover: HTMLInputElement | null = null
 
   async function shortcutHandler(e: HTMLElementEventMap["keydown"]) {
-    if (e.key === "k" && (e.ctrlKey || e.metaKey) && !e.shiftKey) {
+    if (e.key === "/") {
       e.preventDefault()
       const searchBarOpen = container?.classList.contains("active")
       searchBarOpen ? hideSearch() : showSearch("basic")
