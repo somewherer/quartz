2695ed8950648f5946878507d17ab42e8673800d
i <i>
2024-10-28 21:13:59 +1100

    feature: add footer random quote

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -11,11 +11,11 @@ export default ((opts?: Options) => {
   const Footer: QuartzComponent = ({ displayClass, cfg }: QuartzComponentProps) => {
     const year = new Date().getFullYear()
     const links = opts?.links ?? []
+    const sayings: Array<string> = ["What's most valuable is not your time. It's how you spend your attention.","Ultimately, I'm just too fascinated by what everyone is thinking.","We need to take over the world, with all our life. Even if your goal isn't to take over the world, it's still a goal; and if you die, you need to look back and say: I tried.","A friend, I believe, is someone you can meet in a park and invite to your picnic blanket.","Carrot smoothies are very yum.","Imagine not having AI and having to write everything yourself.","Your body is your only home.","There's plenty of beauty here too."];
     return (
       <footer class={`${displayClass ?? ""}`}>
         <p>
-          {i18n(cfg.locale).components.footer.createdWith}{" "}
-          <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year}
+          {sayings[Math.floor(Math.random() * sayings.length)]}
         </p>
         <ul>
           {Object.entries(links).map(([text, link]) => (
