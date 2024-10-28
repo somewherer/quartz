0018350d270b5f002e75dc8761856d5043fae9c1
i <i>
2024-10-28 21:26:46 +1100

    style: graph view config

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -33,8 +33,34 @@ export const defaultContentPageLayout: PageLayout = {
   right: [
     Component.MobileOnly(Component.TableOfContents()),
     Component.MobileOnly(Component.Backlinks()),
-    Component.Graph(),
-  ],
+    Component.Graph({
+      localGraph: {
+        drag: true, // whether to allow panning the view around
+        zoom: true, // whether to allow zooming in and out
+        depth: 1, // how many hops of notes to display
+        scale: 1.5, // default view scale
+        repelForce: 2, // how much nodes should repel each other
+        centerForce: 0.3, // how much force to use when trying to center the nodes
+        linkDistance: 20, // how long should the links be by default?
+        fontSize: 0.3, // what size should the node labels be?
+        opacityScale: 5, // how quickly do we fade out the labels when zooming out?
+        removeTags: [], // what tags to remove from the graph
+        showTags: true, // whether to show tags in the graph
+      },
+      globalGraph: {
+        drag: true, // whether to allow panning the view around
+        zoom: true, // whether to allow zooming in and out
+        depth: 4, // how many hops of notes to display
+        scale: 1.6, // default view scale
+        repelForce: 2, // how much nodes should repel each other
+        centerForce: 0.8, // how much force to use when trying to center the nodes
+        linkDistance: 40, // how long should the links be by default?
+        fontSize: 0.25, // what size should the node labels be?
+        opacityScale: 5, // how quickly do we fade out the labels when zooming out?
+        removeTags: [], // what tags to remove from the graph
+        showTags: true, // whether to show tags in the graph
+      },
+    }),  ],
 }
 
 // components for pages that display lists of pages  (e.g. tags or folders)
