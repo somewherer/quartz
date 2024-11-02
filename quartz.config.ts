20547955d3b26c05b49fbf479c40a56c66a520e3
i <i>
2024-11-02 21:07:32 +1100

    update baseurl and pictures

--- quartz.config.ts
+++ quartz.config.ts
@@ -9,7 +9,7 @@ const config: QuartzConfig = {
     enablePopovers: true,
     analytics: null,
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "somewherer.com",
     ignorePatterns: ["assets/*.md", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
