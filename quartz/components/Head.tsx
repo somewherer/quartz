0b96f8e054f1555ee3deffaeedc47a8718d8349b
i <i>
2024-11-03 11:38:25 +1100

    edit Head.tsx

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -17,13 +17,34 @@ export default (() => {
     const path = url.pathname as FullSlug
     const baseDir = fileData.slug === "404" ? path : pathToRoot(fileData.slug!)
 
-    const iconPath = joinSegments(baseDir, "static/icon.png")
-    const ogImagePath = `https://${cfg.baseUrl}/static/og-image.png`
+    const iconPath = joinSegments(baseDir, "static/icon.jng")
+    const ogImagePath = `https://${cfg.baseUrl}/static/og-image.jng`
 
     return (
       <head>
         <title>{title}</title>
-        <meta charSet="utf-8" />
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width">
+        <meta name="theme-color" content="#ffffff">
+
+        <meta name="description" content={description} />
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+        
+        <meta property="og:width" content="1200" />
+        <meta property="og:height" content="630" />
+        {cfg.baseUrl && <meta property="og:image" content={ogImagePath} />}
+        <link rel="icon" href={iconPath} />
+        
+        <meta content="https://somewherer.com" property="og:url"/>
+        <meta property="og:type" content="website"/>
+
+        <meta property="twitter:title" content="somewherer">
+        <meta property="twitter:description" content="a journey">
+        <meta property="twitter:url" content="https://somewherer.com">
+        <meta property="twitter:card" content="what's a card?">
+        
+        
         {cfg.theme.cdnCaching && cfg.theme.fontOrigin === "googleFonts" && (
           <>
             <link rel="preconnect" href="https://fonts.googleapis.com" />
@@ -31,15 +52,10 @@ export default (() => {
             <link rel="stylesheet" href={googleFontHref(cfg.theme)} />
           </>
         )}
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta property="og:title" content={title} />
-        <meta property="og:description" content={description} />
-        {cfg.baseUrl && <meta property="og:image" content={ogImagePath} />}
-        <meta property="og:width" content="1200" />
-        <meta property="og:height" content="675" />
-        <link rel="icon" href={iconPath} />
-        <meta name="description" content={description} />
-        <meta name="generator" content="Quartz" />
+        
+        
+        <link rel="me" href="https://mastodon.social/@somewherer">
+        
         {css.map((href) => (
           <link key={href} href={href} rel="stylesheet" type="text/css" spa-preserve />
         ))}
